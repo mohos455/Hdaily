3dd41770525da17e450b7051c8a8f911fbf7c285
Mohamed Hesham <106884579+mohos455@users.noreply.github.com>
2022-06-07 19:57:47 +0200

    Update FeedbackForm.vue

--- components/FeedbackForm.vue
+++ components/FeedbackForm.vue
@@ -15,8 +15,6 @@
       <input type="hidden" name="form-name" value="feedback" />
       <label for="name">Name</label>
       <input id="name" type="text" name="name" />
-      <label for="email">Email</label>
-      <input id="email" type="email" name="email" required />
       <label for="feedback">What is your feedback?</label>
       <textarea id="feedback" wrap="soft" name="feedback" required></textarea>
       <button type="submit">Submit</button>
